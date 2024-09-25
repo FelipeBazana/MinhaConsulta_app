b9c4a3e3bce278791e586756009212dabf723302
FelipeBazana <feliebazana@yahoo.com.br>
2024-09-25 08:32:11 -0300

    9252024.1

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,8 +4,8 @@ import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import ConsultationsListScreen from '../screens/ConsultationsListScreen';
-import ConfirmAppointmentScreen from '../screens/ConfirmAppointmentScreen';
 import ScheduleConsultationScreen from '../screens/ScheduleConsultationScreen';
+import ConfirmAppointmentScreen from '../screens/ConfirmAppointmentScreen';
 
 // Definindo o RootStackParamList com todas as telas do projeto
 export type RootStackParamList = {
